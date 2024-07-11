9f5a298fe3d84e7987adfcdaa9e07825f97ddca2
Yuvraj049 <yvc3094@gmail.com>
2024-07-11 13:52:57 +0530

    conducted some snapshot tests

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,21 +19,21 @@ function Navbar(props) {
   };
   return (
     <div>
-      <nav class="bg-gray-800">
-        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-3">
-          <div class="relative flex h-20 items-center justify-center">
-            {/* <div class="relative flex flex-1"> */}
-              <div class="absolute left-0 flex flex-shrink-0 items-center">
-                {/* <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" /> */}
-                {/* <img class="h-8 w-auto" src="" alt="Your Company" /> */}
-                <img class="h-10 w-auto" src=".././IRCTC-Color.svg" alt="" />
+      <nav className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-3">
+          <div className="relative flex h-20 items-center justify-center">
+            {/* <div className="relative flex flex-1"> */}
+              <div className="absolute left-0 flex flex-shrink-0 items-center">
+                {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" /> */}
+                {/* <img className="h-8 w-auto" src="" alt="Your Company" /> */}
+                <img className="h-10 w-auto" src=".././IRCTC-Color.svg" alt="" />
               </div>
-              <div class="relative flex justify-center">
-              <div class="hidden sm:block">
-                <div class="flex space-x-4">
+              <div className="relative flex justify-center">
+              <div className="hidden sm:block">
+                <div className="flex space-x-4">
                   {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                   {
-                    props.navbar.map((element)=>{
+                    props.navbar && props.navbar.map((element)=>{
                       return(
                         <>
                           <Link className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" to={element[1]}>{element[0]}</Link>
@@ -46,23 +46,22 @@ function Navbar(props) {
                 </div>
               </div>
               </div>
-              <div class="absolute right-1 sm:ml-6 sm:pr-0">
-                <div class="relative ml-3">
+              <div className="absolute right-1 sm:ml-6 sm:pr-0">
+                <div className="relative ml-3">
                   <div>
-                    <Link to="/profile"><button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                      <img class="h-8 w-8 rounded-full" src={user && user.photoURL ? user.photoURL : url} alt="" />
+                    <Link to="/profile"><button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                      <img className="h-8 w-8 rounded-full" src={user && user.photoURL ? user.photoURL : url} alt="" />
                     </button></Link>
                   </div>
                 </div>
               </div>
             {/* </div> */}
           </div>
-
           {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-          <div class="sm:hidden" id="mobile-menu">
-            <div class="space-y-1 px-2 pb-3 pt-2">
+          <div className="sm:hidden" id="mobile-menu">
+            <div className="space-y-1 px-2 pb-3 pt-2">
             {
-                    props.navbar.map((element)=>{
+                    props.navbar && props.navbar.map((element)=>{
                       return(
                         <>
                           <Link className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" to={element[1]}>{element[0]}</Link><br/>
